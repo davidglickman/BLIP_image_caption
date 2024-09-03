bd99a7decdc20706f92e1281261c206bbfa3ecb6
davidglickman <37016306+davidglickman@users.noreply.github.com>
2024-09-03 09:34:14 +0300

    Create BLIP.py

---
+++ BLIP.py
@@ -0,0 +1,34 @@
+import gradio as gr
+import numpy as np
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+
+def caption_image(input_image: np.ndarray):
+    # Convert numpy array to PIL Image and convert to RGB
+    raw_image = Image.fromarray(input_image).convert('RGB')
+    
+    # Process the image
+    inputs = processor(input_image, return_tensors="pt")
+
+    # Generate captions
+    outputs = model.generate(**inputs)
+    
+    # Decode the generated tokens to text and store it into `caption`
+    caption = processor.decode(outputs[0],skip_special_tokens=True)
+
+    return caption
+  
+
+iface = gr.Interface(
+    fn=caption_image, 
+    inputs=gr.Image(), 
+    outputs="text",
+    title="Image Captioning",
+    description="This is a simple web app for generating captions for images using a trained model."
+)
+
+iface.launch()
